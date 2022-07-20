ad1dbdda82ccfec7bf7329167fbda615a24cda6d
Tomoaki Imai <tomoima525@gmail.com>
2022-07-20 10:39:48 -0700

    Generate groth16 VerifierContract (#11)
    * Generate groth16 VerifierContract

--- node-script/generateFinalKey.ts
+++ node-script/generateFinalKey.ts
@@ -1,3 +1,5 @@
+// Referenced from https://github.com/appliedzkp/semaphore/blob/main/scripts/compile-circuits.ts
+
 import fs from 'fs';
 import { zKey } from 'snarkjs';
 import logger from 'js-logger';
@@ -6,7 +8,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function main() {
   const buildPath = './circuit';
-  //const solidityVersion = "0.8.4";
+  const solidityVersion = '0.8.13';
 
   if (!fs.existsSync(buildPath)) {
     fs.mkdirSync(buildPath, { recursive: true });
@@ -21,53 +23,61 @@ async function main() {
   if (stdout) {
     console.log('Compile circuit done', { stdout });
   }
+
+  // Generate zkey from ptau(first contribution, second contribution)
   await zKey.newZKey(
     `${buildPath}/verifytalent.r1cs`,
-    `${buildPath}/powersOfTau28_hez_final_16.ptau`,
+    `${buildPath}/powersOfTau28_hez_final_10.ptau`,
     `${buildPath}/verifytalent_0000.zkey`,
     logger,
   );
 
-  // await zKey.beacon(
-  //   `${buildPath}/verifytalent_0000.zkey`,
-  //   `${buildPath}/verifytalent_final.zkey`,
-  //   "Final Beacon",
-  //   "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
-  //   10,
-  //   logger
-  // );
+  // Apply random beacon
+  await zKey.beacon(
+    `${buildPath}/verifytalent_0000.zkey`,
+    `${buildPath}/verifytalent_final.zkey`,
+    'Final Beacon',
+    '0102030405060708090a0b0c0d0e0f202122232425262728292a2b2c2d2e2f',
+    10,
+    logger,
+  );
 
-  // let verifierCode = await zKey.exportSolidityVerifier(
-  //   `${buildPath}/verifytalent_final.zkey`,
-  //   {
-  //     groth16: fs.readFileSync(
-  //       "./node_modules/snarkjs/templates/verifier_groth16.sol.ejs",
-  //       "utf8"
-  //     ),
-  //   },
-  //   logger
-  // );
-  // verifierCode = verifierCode.replace(
-  //   /pragma solidity \^\d+\.\d+\.\d+/,
-  //   `pragma solidity ^${solidityVersion}`
-  // );
+  // Export verifier code
+  let verifierCode = await zKey.exportSolidityVerifier(
+    `${buildPath}/verifytalent_final.zkey`,
+    {
+      groth16: fs.readFileSync(
+        './node_modules/snarkjs/templates/verifier_groth16.sol.ejs',
+        'utf8',
+      ),
+    },
+    logger,
+  );
 
-  // fs.writeFileSync("./contracts/Verifier.sol", verifierCode, "utf-8");
+  // Replace solidity version
+  verifierCode = verifierCode.replace(
+    /pragma solidity \^\d+\.\d+\.\d+/,
+    `pragma solidity ^${solidityVersion}`,
+  );
 
-  // const verificationKey = await zKey.exportVerificationKey(
-  //   `${buildPath}/verifytalent_final.zkey`,
-  //   logger
-  // );
-  // fs.writeFileSync(
-  //   `${buildPath}/verification_key.json`,
-  //   JSON.stringify(verificationKey),
-  //   "utf-8"
-  // );
+  fs.writeFileSync('./src/TalentVerifier.sol', verifierCode, 'utf-8');
 
-  // fs.renameSync(
-  //   `${buildPath}/verifytalent_js/verifytalent.wasm`,
-  //   `${buildPath}/verifytalent.wasm`
-  // );
+  // Verification key to use on the client side for generating proofs
+  const verificationKey = await zKey.exportVerificationKey(
+    `${buildPath}/verifytalent_final.zkey`,
+    logger,
+  );
+  fs.writeFileSync(
+    `${buildPath}/verification_key.json`,
+    JSON.stringify(verificationKey),
+    'utf-8',
+  );
+
+  fs.renameSync(
+    `${buildPath}/verifytalent_js/verifytalent.wasm`,
+    `${buildPath}/verifytalent.wasm`,
+  );
+  // We will not remove them
   // rimraf.sync(`${buildPath}/verifytalent_js`);
   // rimraf.sync(`${buildPath}/powersOfTau28_hez_final_16.ptau`);
   // rimraf.sync(`${buildPath}/verifytalent_0000.zkey`);
