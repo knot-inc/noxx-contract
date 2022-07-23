d290508e232aab15123084def5ce400141206692
Tomoaki Imai <tomoima525@gmail.com>
2022-07-22 17:18:52 -0700

    A contract that checks if a user is verified to mint nft

---
+++ src/interfaces/INoxx.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+interface INoxx {
+  /// @dev Verify zk proof, if valid then add to the allowed list
+  function executeProofVerification(
+    uint256[8] calldata proof,
+    uint256[1] calldata input,
+    address from
+  ) external returns (bool);
+
+  /// @dev mintNFT if user is in the allowed list
+  function mintNFT(address to, string memory _tokenURI) external;
+}
