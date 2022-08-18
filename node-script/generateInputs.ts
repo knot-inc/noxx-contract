bbdbca60b95bd1a700ba2770c459edd494294784
Tomoaki Imai <tomoima525@gmail.com>
2022-08-18 09:54:02 -0700

    extract merkleProof logic to reuse

--- node-script/generateInputs.ts
+++ node-script/generateInputs.ts
@@ -5,19 +5,28 @@ import {
   createPoseidonHash,
   encodeStringToBigInt,
 } from './zk-tools/lib';
+import { generateMerkleProof } from './utils/generateMerkleProof';
+import { BigNumber } from 'ethers';
 
 async function main() {
   const poseidon = await buildPoseidon();
-  const commitment: bigint = encodeStringToBigInt('John Doe');
+  const name: bigint = encodeStringToBigInt('John Doe');
+  const age: bigint = BigNumber.from(20) as unknown as bigint;
+  const country: bigint = encodeStringToBigInt('JP');
   const nonce: bigint = encodeStringToBigInt('1234');
 
-  const hash = createPoseidonHash(poseidon, [commitment, nonce]);
-  console.log({ commitment, nonce, hash });
+  const commits = [name, age, country].map((v) =>
+    createPoseidonHash(poseidon, [v, nonce]).toString(),
+  );
+  const merkleProof = await generateMerkleProof('JP');
   const input = {
-    values: [commitment.toString()],
-    nonces: [nonce.toString()],
-    commits: [hash],
+    values: [name, age, country].map((v) => v.toString()),
+    nonces: [nonce.toString(), nonce.toString(), nonce.toString()],
+    commits,
+    age: 18,
+    ...merkleProof,
   };
+  console.log({ input });
   // write to file
   const inputPath = path.join(__dirname, `/../circuit/`, 'input.json');
   if (fs.existsSync(inputPath)) {
